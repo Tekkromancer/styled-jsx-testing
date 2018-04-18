78c4eaa687c7210c625dd27ea55d3ebceabbe7e9
Scott David Garson <scott_garson@mckinsey.com>
2018-04-18 13:46:18 -0400

    Cleanup

--- pages/list.js
+++
@@ -1,6 +0,0 @@
-import { React } from 'common_imports';
-import PageComponent, { getInitialProps } from '../app/pages/ListPage/ListPage';
-
-const Page = props => <PageComponent {...props} />;
-Page.getInitialProps = getInitialProps;
-export default Page;
--- pages/post.js
+++
@@ -1,6 +0,0 @@
-import { React } from 'common_imports';
-import PageComponent, { getInitialProps } from '../app/pages/PostPage/PostPage';
-
-const Page = props => <PageComponent {...props} />;
-Page.getInitialProps = getInitialProps;
-export default Page;
